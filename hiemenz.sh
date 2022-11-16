5daf0f18956a8a99aab325d131b819b57186a23c
Joshua F. Robinson <joshuarrr@protonmail.com>
2022-11-16 15:44:05 +0100

    Hiemenz script now runs standalone without piping to another bash shell, data files are generated in proper subdirectory and ignored by git.

--- hiemenz.sh
+++ hiemenz.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # hiemenz.sh -- automate generation of data files
 
@@ -20,42 +20,38 @@
 # The code generates commands which can be executed by piping the
 # output into /usr/bin/bash.  If necessary this can be filtered, eg:
 
-# ./hiemenz.sh | /usr/bin/bash
-# ./hiemenz.sh | grep delta | /usr/bin/bash
-# ./hiemenz.sh | grep reynolds | /usr/bin/bash
-
 # Boundary layer thickness control (original case)
 
 stokes=0.2,1.0,0.01
 for x in 000 001 002 005 010 020
 do
-    echo "./hiemenz.py --delta=$x/100 --stokes=$stokes -o hiemenz$x.dat"
+    ./hiemenz.py --delta=$x/100 --stokes=$stokes -o data/hiemenz$x.dat
 done
 
 stokes=0.2,1.5,0.01
 x=050
-echo "./hiemenz.py --delta=$x/100 --stokes=$stokes -o hiemenz$x.dat"
+./hiemenz.py --delta=$x/100 --stokes=$stokes -o data/hiemenz$x.dat
 
 stokes=0.5,2.0,0.01
 x=100
-echo "./hiemenz.py --delta=$x/100 --stokes=$stokes -o hiemenz$x.dat"
+./hiemenz.py --delta=$x/100 --stokes=$stokes -o data/hiemenz$x.dat
 
 # Reynolds number control
 
 stokes=0.25,1.0,0.01
 x=inft
-echo "./hiemenz.py --reynolds=$x --stokes=$stokes -o reynolds$x.dat"
+./hiemenz.py --reynolds=$x --stokes=$stokes -o data/reynolds$x.dat
 
 stokes=0.20,1.0,0.01
 for x in 0005 0010 0050 0100 0200 0500 1000 2000 5000
 do
-    echo "./hiemenz.py --reynolds=$x --stokes=$stokes -o reynolds$x.dat"
+    ./hiemenz.py --reynolds=$x --stokes=$stokes -o data/reynolds$x.dat
 done
 
 stokes=0.20,1.5,0.01
 x=0002
-echo "./hiemenz.py --reynolds=$x --stokes=$stokes -o reynolds$x.dat"
+./hiemenz.py --reynolds=$x --stokes=$stokes -o data/reynolds$x.dat
 
 stokes=0.50,2.0,0.01
 x=0001
-echo "./hiemenz.py --reynolds=$x --stokes=$stokes -o reynolds$x.dat"
+./hiemenz.py --reynolds=$x --stokes=$stokes -o data/reynolds$x.dat
