64f623e00419ad929d9cd413480a432f1240fd10
Joshua F. Robinson <joshuarrr@protonmail.com>
2023-12-19 12:47:00 +0000

    Split singular onset figure into two: one for power-law flow field and one for Hiemenz flow.

--- show-singular-onset.py
+++ show-singular-onset.py
@@ -19,6 +19,7 @@ def adjust_lightness(color, amount):
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
 
+lw = 0.75
 cmap = plt.get_cmap('turbo_r')
 paths_hiemenz = natsorted(glob('data/efficiency_hiemenz*.csv'))
 paths_power = glob('data/efficiency_power*.csv')
@@ -28,15 +29,11 @@ for p in paths_power: print(p)
 # paths_power = natsorted(glob('data/efficiency_power*.csv'))
 # for p in paths_power: print(p)
 
-figsize1 = (3.375, 2.1)
-figsize2 = (3.375, 2.5)
-fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(figsize1[0], 2*figsize1[1]), constrained_layout=True)
-fig2, (ax3, ax4) = plt.subplots(nrows=2, figsize=(figsize2[0], 2*figsize2[1]), constrained_layout=True, sharex=True, sharey=True, gridspec_kw={'hspace': 0})
-#fig2.subplots_adjust(hspace=0)
-# plt.figure(figsize=figsize2)
-# ax3 = plt.gca()
-# plt.figure(figsize=figsize2)
-# ax4 = plt.gca()
+figsize = (3.375, 2.5)
+fig1, (ax3, ax1) = plt.subplots(nrows=2, figsize=(figsize[0], 2*figsize[1]), constrained_layout=True)
+fig2, (ax4, ax2) = plt.subplots(nrows=2, figsize=(figsize[0], 2*figsize[1]), constrained_layout=True)
+
+# Show collection efficiencies for Hiemenz model (ax1):
 
 St_cross= []
 eff_cross = []
@@ -65,20 +62,16 @@ for i, path in enumerate(paths_hiemenz):
      # default colour scheme is very bright which obscures some of the lighter colours  (esp. greens)
     c = adjust_lightness(c, 0.75)
 
-    for ax in [ax2, ax3]: ax.plot(dSt, eff, c=c, lw=0.75, label=Re_str)
+    ax1.plot(dSt, eff, c=c, lw=lw, label=Re_str)
 
-    St = np.concatenate([[Stc_hiemenz[Re]], St])
-    eff = np.concatenate([[0], eff])
-    ax1.plot(St, eff, c=c, lw=0.75, label=Re_str)
+# Limiting inviscid efficiencies for Hiemenz (ax1) and power law (ax2) models:
 
 St, dSt, eff = np.genfromtxt('data/efficiency_shm.csv', skip_header=1).T
-for ax in [ax2, ax3]:
-    ax.plot(dSt, eff, c='k', lw=0.75, label='$\infty$')
-    cross_plot, = ax.plot(St_cross, eff_cross, 'k--')
-ax4.plot(dSt, eff, c='k', lw=0.75, label='1.0')
-St = np.concatenate([[0.25], St])
-eff = np.concatenate([[0], eff])
-ax1.plot(St, eff, c='k', lw=0.75, label='$\infty$')
+ax1.plot(dSt, eff, c='k', lw=lw, label='$\infty$')
+cross_plot, = ax1.plot(St_cross, eff_cross, 'k--')
+ax2.plot(dSt, eff, c='k', lw=lw, label='1.0')
+
+# Show collection efficiencies for power law model (ax2):
 
 Stc_power = {}
 Stc_power[1] = 0.25
@@ -92,63 +85,35 @@ for i, path in enumerate(paths_power):
     Stc_power[m] = np.average(St - dSt)
 
     c = cmap(i/(len(paths_power)-1))
-     # default colour scheme is very bright which obscures some of the lighter colours  (esp. greens)
+    # default colour scheme is very bright which obscures some of the lighter colours  (esp. greens)
     c = adjust_lightness(c, 0.75)
 
-    ax4.plot(dSt, eff, c=c, lw=0.75, label=m_str)
+    ax2.plot(dSt, eff, c=c, lw=lw, label=m_str)
 
-ax4.set_xscale('log')
-ax4.set_yscale('log')
+# Formatting/legends of efficiencies plots:
 
-# for ax in [ax1, ax2]:
-#     #ax.legend(loc='upper left', title='Re', title_fontsize=8, alignment='left', ncol=2, fontsize=8, borderpad=0)
-#     ax.legend(loc='lower right', title='Re', title_fontsize=8, alignment='left', ncol=2, fontsize=6, borderpad=0)
-#     ax.set_xlabel('$\mathrm{St}$')
-#     #ax2.set_ylabel('squared efficiency $\lambda^2$')
-#     ax.set_ylabel('efficiency $\lambda$')
-#     #ax.set_xlim([0, 2])
-#     #ax.set_ylim([0, 0.35])
-#     ax.set_xscale('log')
-#     ax.set_yscale('log')
-
-for ax in [ax1, ax2, ax3, ax4]:
+for ax in [ax1, ax2]:
     ax.set_ylabel('efficiency $\lambda = 2y_c$')
-
-ax1.set_xlabel('$\mathrm{St}$')
-#ax2.set_ylabel('squared efficiency $\lambda^2$')
-
-ax1.legend(loc='lower right', title='Re', title_fontsize=8, alignment='center', ncol=3, fontsize=8, borderpad=0, labelspacing=0., handlelength=1.)
-ax1.set_xlim([0, 2])
-ax1.set_ylim([0, 0.35])
-
-for ax in [ax2, ax4]:
     ax.set_xlabel('$\mathrm{St} - \mathrm{St}_c$')
-for ax in [ax2, ax3, ax4]:
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_ylim([1e-5, 0.5])
     ax.set_xlim([1e-5, 1])
 
-# ax4.set_xlabel('$\mathrm{St} - \mathrm{St}_c$')
-# ax4.set_xscale('log')
-# ax4.set_yscale('log')
-# ax4.set_xlim([1e-7, 1])
-# ax4.set_ylim([1e-7, 1])
-
-leg3 = ax3.legend(loc='upper left', title='Re', title_fontsize=8, alignment='center', ncol=3, fontsize=8, borderpad=0, labelspacing=0., handlelength=1.)
-ax4.legend(loc='upper left', title='$m$', title_fontsize=8, alignment='center', ncol=3, fontsize=8, borderpad=0, labelspacing=0., handlelength=1.)
+leg1 = ax1.legend(loc='upper left', title='Re', title_fontsize=8, alignment='center', ncol=3, fontsize=8, borderpad=0, labelspacing=0., handlelength=1.)
+ax2.legend(loc='upper left', title='$m$', title_fontsize=8, alignment='center', ncol=3, fontsize=8, borderpad=0, labelspacing=0., handlelength=1.)
 
 St, dSt, eff = np.genfromtxt('data/efficiency_kuwabara_alpha=0.15_2.csv', skip_header=1).T
-ax1.plot(St, eff, '-.')
-for ax in [ax2, ax3]:
-    kuwabara_plot, = ax.plot(dSt, eff, '-.', lw=0.75)
-    ax.legend([kuwabara_plot, cross_plot], [r'$\mathrm{Re}=0$ (Kuwabara $\alpha=0.15$)', r'$\lambda = \mathrm{Re}^{-1/2}$'],
-              loc='lower left', fontsize=6, borderpad=-0.25, labelspacing=0., handlelength=2.1)
-ax3.add_artist(leg3)
+kuwabara_plot, = ax1.plot(dSt, eff, '-.', lw=lw)
+ax1.legend([kuwabara_plot, cross_plot],
+           [r'$\mathrm{Re}=0$ (Kuwabara $\alpha=0.15$)', r'$\lambda = \mathrm{Re}^{-1/2}$'],
+           loc='lower left', fontsize=6, borderpad=-0.25, labelspacing=0., handlelength=2.1)
+ax1.add_artist(leg1)
+
+# Annotate the square-root slopes of the small inertia regions, and the inviscid limits:
 
 f = lambda x,c: c*x**0.5
-for ax in [ax2, ax3, ax4]:
-    #x, c = 2e-5, 5e-3
+for ax in [ax1, ax2]:
     x, c = 1e-4, 0.6
     annotation.slope_marker((x, f(x, c)), (1, 2), invert=True, ax=ax,
                             poly_kwargs=dict(ec='black', fill=False, lw=0.5),
@@ -159,86 +124,41 @@ for ax in [ax2, ax3, ax4]:
                  xytext=(7*x, y), xy=(x, y),
                  arrowprops=dict(facecolor='black', lw=0.5, arrowstyle='->'))
 
-# x, c = 2e-7, 5e-3
-# annotation.slope_marker((x, f(x, c)), (1, 2), invert=False, ax=ax4,
-#                         poly_kwargs=dict(ec='black', fill=False, lw=0.5),
-#                         text_kwargs=dict(fontsize=8))
-# x, y = 1.5e-2, 1e-4
-# ax4.annotate('inviscid\nscaling', size=8, va='center', ha='center',
-#              xytext=(20*x, y), xy=(x, y),
-#              arrowprops=dict(facecolor='black', lw=0.5, arrowstyle='->'))
+# Show the critical Stokes numbers for the Hiemenz flow (ax3) and the power law model (ax4)
 
-for ax, figsize, gap, inset_width in zip([ax2, ax3], [figsize1, figsize2],
-                                         [0.05, 0.04], [0.3, 0.225]):
-    #inset_width = 0.3
-    aspect = figsize[1] / figsize[0]
-    #inset = ax.inset_axes([gap*aspect, 1-gap - inset_width, inset_width, inset_width])
-    inset = ax.inset_axes([1 - inset_width - gap*aspect, gap, inset_width, inset_width])
+Re,Stc1 = np.array([(k,v) for k,v in Stc_hiemenz.items()]).T
+ax3.semilogx(Re, Stc1, lw=lw)
+ax3.set_ylim([0, 1])
+ax3.axhline(y=0.25, ls='dotted')
+ax3.set_xlim([1, 1e8])
 
-    Re,Stc1 = np.array([(k,v) for k,v in Stc_hiemenz.items()]).T
-    inset.semilogx(Re, Stc1)
-    inset.set_ylim([0, 1])
-    inset.axhline(y=0.25, ls='dotted')
+xticks = np.geomspace(1, 1e8, 9)
+ax3.set_xticks(xticks)
+labels = ax3.get_xticklabels()
+labels[1::2] = ['' for _ in labels[1::2]]
+ax3.set_xticklabels(labels)
 
-    xticks = np.geomspace(1, 1e8, 9)
-    inset.set_xticks(xticks)
-    labels = inset.get_xticklabels()
-    labels[1:-1] = ['' for _ in labels[1:-1]]
-    inset.set_xticklabels(labels, fontsize=8)
+m,Stc1 = np.array([[(1,0)] + [(k,v) for k,v in Stc_power.items()]]).T
+ax4.plot(m, Stc1, lw=lw)
+ax4.set_xlim([0, 2])
+ax4.set_ylim([0, 0.6])
+ax4.axhline(y=0.25, ls='dotted')
 
-    yticks = np.linspace(0, 1, 5)
-    inset.set_yticks(yticks)
-    labels = ['%.1f' % y for y in yticks]
-    labels[1:-1] = ['' for _ in labels[1:-1]]
-    inset.set_yticklabels(labels, fontsize=8)
+for ax in [ax3, ax4]: ax.set_ylabel('$\mathrm{St}_\mathrm{c}$')
+ax3.set_xlabel('$\mathrm{Re}$')
+ax4.set_xlabel('$m$')
 
-    inset.set_xlabel('$\mathrm{Re}$', labelpad=-3, fontsize=8)
-    inset.set_ylabel('$\mathrm{St}_\mathrm{c}$', labelpad=-3, fontsize=8)
-
-    inset.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
-    inset.xaxis.set_label_position('top')
-
-figsize = figsize2
-ax = ax4
-inset_width = 0.225
-if True:
-    #inset_width = 0.3
-    aspect = figsize[1] / figsize[0]
-    #inset = ax.inset_axes([gap*aspect, 1-gap - inset_width, inset_width, inset_width])
-    inset = ax.inset_axes([1 - inset_width - gap*aspect, gap, inset_width, inset_width])
-
-    m,Stc1 = np.array([(k,v) for k,v in Stc_power.items()]).T
-    inset.plot(m, Stc1)
-    inset.set_xlim([1, 2])
-    inset.set_ylim([0, 0.6])
-    inset.axhline(y=0.25, ls='dotted')
-
-    inset.set_xticks([1,1.5,2])
-    labels = inset.get_xticklabels()
-    labels[1:-1] = ['' for _ in labels[1:-1]]
-    inset.set_xticklabels(labels, fontsize=8)
-
-    inset.set_yticks([0, 0.25, 0.5])
-    inset.set_yticklabels(['0', '', '0.5'], fontsize=8)
-
-    inset.set_xlabel('$m$', labelpad=-3, fontsize=8)
-    inset.set_ylabel('$\mathrm{St}_\mathrm{c}$', labelpad=-3, fontsize=8)
-
-    inset.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
-    inset.xaxis.set_label_position('top')
-
-x, y, fontsize, ha, va = 0.025, 0.825, 18, 'left', 'bottom'
-for ax, letter in zip([ax1, ax2], 'ab'):
-    label = ax.text(x, y, r'\textbf{%s}' % letter, transform=ax.transAxes, zorder=20,
-                     fontsize=fontsize, horizontalalignment=ha, verticalalignment=va)
-    label.set_in_layout(False)
+# Letter each subpanel:
 
 x, y, fontsize, ha, va = -0.175, 0.9, 18, 'left', 'bottom'
-for ax, letter in zip([ax3, ax4], 'ab'):
-    label = ax.text(x, y, r'\textbf{%s}' % letter, transform=ax.transAxes, zorder=20,
-                     fontsize=fontsize, horizontalalignment=ha, verticalalignment=va)
-    label.set_in_layout(False)
+for axes in [[ax3, ax1], [ax4, ax2]]:
+    for ax, letter in zip(axes, 'ab'):
+        label = ax.text(x, y, r'\textbf{%s}' % letter, transform=ax.transAxes, zorder=20,
+                        fontsize=fontsize, horizontalalignment=ha, verticalalignment=va)
+        label.set_in_layout(False)
 
-plt.savefig('singular-onset.pdf')
-plt.savefig('singular-onset.png')
-plt.show()
+fig1.savefig('singular-onset-hiemenz.pdf')
+fig1.savefig('singular-onset-hiemenz.png')
+fig2.savefig('singular-onset-power.pdf')
+fig2.savefig('singular-onset-power.png')
+# plt.show()
