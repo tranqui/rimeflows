18d36f3f98a59a36be24bb2f82576c02d6bc1ff5
Joshua F. Robinson <joshuarrr@protonmail.com>
2024-04-15 16:32:10 +0100

    Improvements to make chord sketch more pretty.

--- sketch-chord.py
+++ sketch-chord.py
@@ -15,15 +15,42 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np, matplotlib.pyplot as plt
-from flows import chord
+from matplotlib.patches import Circle
+from flows import kuwabara, chord
 
 from arrows import add_arrow
 plt.style.use('figstyle.mplstyle')
 
-# fig, axes = plt.subplots(nrows=2)
-# ax1, ax2 = axes
-ax2 = plt.gca()
-axes = np.array([ax2])
+fig, axes = plt.subplots(nrows=2)
+ax1, ax2 = axes
+# ax2 = plt.gca()
+# axes = np.array([ax2])
+
+
+## First plot: flow around a circle.
+
+volume_fraction = 0.05
+circle_flow = kuwabara.FlowField(volume_fraction)
+
+L = circle_flow.l
+x0 = -L
+for y0 in np.linspace(-0.2*L, 0.2*L, 4):
+    x, y = circle_flow.streamline_cartesian([x0, y0], xmax=1.2*L, ymax=1.2*L)
+    select = np.abs(x) < L
+    x, y = x[select], y[select]
+    pl, = ax1.plot(x, y, 'k-', lw=0.5)
+
+    for x in [-0.5*L, 0.5*L]:
+        add_arrow(pl, x=x, zorder=-1)
+
+ax1.add_patch(Circle((0,0), 1, fc='r', ec='None', zorder=10))
+
+ax1.set_xlim([-1.2*L, 1.2*L])
+ax1.set_ylim([-0.5*L, 0.5*L])
+ax1.set_aspect(1)
+
+
+## Second plot: flow around a chord.
 
 chord_length = 1
 alpha = np.pi/2 - 0.25*np.pi
@@ -42,8 +69,8 @@ x_chord, y_chord = (R * np.array((x_chord, y_chord))).A
 L = 1.5
 eps = 1e-6
 x0 = -L
-for y0 in np.linspace(-3*L, 3*L, 21):
-    x, y = chord_flow.streamline([x0, y0], max_step=0.1, ymax=(3*L+eps))
+for y0 in np.linspace(-0.5*chord_length, 0.5*chord_length, 4):
+    x, y = chord_flow.streamline(([x0, y0] * R).tolist()[0], max_step=0.1, ymax=(3*L+eps))
     x, y = (R * np.array((x, y))).A
     pl, = ax2.plot(x, y, 'k-', lw=0.5)
 
@@ -57,9 +84,9 @@ ax2.plot([0, x_chord[-1]], [0, 0], '--', c=pl.get_color())
 th_arc = np.linspace(0, alpha, 1000)
 r_arc = 0.25 * chord_length
 ax2.plot(r_arc * np.cos(th_arc), r_arc * np.sin(th_arc), '-', c=pl.get_color())
-r_arc *= 1.3
+r_arc *= 1.4
 ax2.text(r_arc * np.cos(alpha/2), r_arc * np.sin(alpha/2), r'$\alpha$',
-         fontsize=10, c=pl.get_color(), ha='center', va='center')
+         fontsize=14, c=pl.get_color(), ha='center', va='center')
 
 # Stc = 0.12583899
 # x0, y0, St = -1, -0.078, Stc + 1e-3
@@ -73,11 +100,21 @@ ax2.text(r_arc * np.cos(alpha/2), r_arc * np.sin(alpha/2), r'$\alpha$',
 # if collides: ax2.plot(x[-1], y[-1], 'o', c=pl.get_color())
 
 ax2.set_xlim([-L,L])
-ax2.set_ylim([-L,L])
+# ax2.set_ylim([-L,L])
+ax2.set_ylim([-0.5*L,0.5*L])
 ax2.set_aspect(1)
+
 
 for ax in axes.ravel():
     ax.set_xticks([])
     ax.set_yticks([])
+    for spine in ax.spines.values():
+        spine.set_visible(False)
 
+for ax, y, letter in zip(axes, [0.9, 0.45], 'ab'):
+    label = ax.text(0.05, y, r'\textbf{%s}' % letter, transform=fig.transFigure, zorder=20,
+                    fontsize=18, horizontalalignment='left', verticalalignment='bottom')
+    label.set_in_layout(False)
+
+plt.savefig('sketch-obstacles.pdf')
 plt.show()
